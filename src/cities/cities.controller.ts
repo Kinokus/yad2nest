aec7074bde65e91cc3a88b2bdc9060916eeab2cb
kinokus <aleks.kinokus@gmail.com>
2020-11-26 10:24:17 +0200

    controller + city

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,20 +1,44 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Redirect, Req, Res,
+} from '@nestjs/common';
 import { CreateCityDto } from './dto/create-city.dto';
+import { City } from './classes/city';
+import { CreateStatusesEnum } from './classes/create-statuses.enum';
+import { UpdateCityDto } from './dto/update-city.dto';
+import { UpdateStatusesEnum } from './classes/update-statuses.enum';
+import { DeleteStatusesEnum } from './classes/delete-statuses.enum';
 
-class City {
-}
+import {Response, Request } from 'express'
 
 @Controller('cities')
 export class CitiesController {
 
   @Get()
   getCities() {
+
     return [];
   }
 
+  // @Get()
+  // // @Redirect('https://google.com', 301)
+  // getCities(@Req() req: Request, @Res() res: Response) {
+  //   res.status(201).end('ppp')
+  //   return [];
+  // }
+
   @Get(':id')
   getOne(@Param('id') id: string): City {
-    return { id };
+    return { id, name: '' };
   }
 
   // @Get(':id')
@@ -23,17 +47,24 @@ export class CitiesController {
   // }
 
   @Post()
-  create(@Body() createCityDto: CreateCityDto) {
-    console.log(createCityDto);
-    return { ...createCityDto, status: 'added' };
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cache-Control', 'none')
+  create(@Body() createCityDto: CreateCityDto): CreateCityDto {
+    return { ...createCityDto, status: CreateStatusesEnum.Success };
   }
 
-  @Delete()
-  remove() {
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return{
+      id, status:DeleteStatusesEnum.Success
+    }
   }
 
-  @Put()
-  update() {
+  @Put(':id')
+  update(@Body() updateCityDto: UpdateCityDto, @Param('id') id: string): UpdateCityDto {
+    return {
+      ...updateCityDto, id, status: UpdateStatusesEnum.Success,
+    };
   }
 
 }
