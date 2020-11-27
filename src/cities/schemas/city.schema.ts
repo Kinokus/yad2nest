af30cf293e3d3b6e3f13d393c9530152a1d78ade
kinokus <aleks.kinokus@gmail.com>
2020-11-27 19:24:20 +0200

    schema

---
+++ src/cities/schemas/city.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+export type CityDocument = City & Document
+
+
+@Schema()
+export class City {
+  @Prop()
+  name: string
+
+  @Prop()
+  id: string
+
+}
+
+export const CitySchema = SchemaFactory.createForClass(City)
