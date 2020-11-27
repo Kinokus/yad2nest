b0606da24dc6245c6f18dc2873d3d9321a874516
kinokus <aleks.kinokus@gmail.com>
2020-11-27 19:24:49 +0200

    temp

---
+++ src/apartments/apartments.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+
+class City {
+  id: string;
+}
+
+@Controller('apartments')
+export class ApartmentsController {
+
+}
+
